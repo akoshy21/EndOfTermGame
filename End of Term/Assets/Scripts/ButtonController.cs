19002fc77e0da7fd1da42e4c7e2f556f499b4b7a
Unknown <rmh472@nyu.edu>
2018-04-27 13:46:02 -0400

    Battle Options, starting on getting a system of buttons that change text

---
+++ End of Term/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+    public int button;
+    public Button but;
+
+	void Start () {
+
+	}
+
+	void Update () {
+        UpdateText();
+	}
+
+    void UpdateText()
+    {
+        switch (button)
+        {
+            case 1:
+                if (GameManager.manager.turnstate == GameManager.TurnState.Menu)
+                {
+                    but.GetComponentInChildren<Text>().text = "Attack";
+                }
+                return;
+            case 2:
+                if (GameManager.manager.turnstate == GameManager.TurnState.Menu)
+                {
+                    but.GetComponentInChildren<Text>().text = "Characters";
+                }
+                return;
+            case 3:
+                if (GameManager.manager.turnstate == GameManager.TurnState.Menu)
+                {
+                    but.GetComponentInChildren<Text>().text = "Attack";
+                }
+                return;
+            case 4:
+                if (GameManager.manager.turnstate == GameManager.TurnState.Menu)
+                {
+                    but.GetComponentInChildren<Text>().text = "Attack";
+                }
+                return;
+            default:
+                return;
+        }
+    }
+}
---
+++ End of Term/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager manager;
+
+    public enum TurnState { Menu, Attacks, CharacterSwap, }
+    public TurnState turnstate;
+
+    void Awake () {
+        GameManagerSetup();
+	}
+
+    void Start()
+    {
+        turnstate = TurnState.Menu;
+    }
+
+
+    void Update () {
+		
+	}
+
+    void GameManagerSetup()
+    {
+        if (manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
