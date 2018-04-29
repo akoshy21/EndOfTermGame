f8cc2812ead7772605da2cfa047c7aa59d580f87
Unknown <ak6429@nyu.edu>
2018-04-28 21:26:09 -0400

    more comments

--- End of Term/Assets/Scripts/CharacterSwapButton.cs
+++ End of Term/Assets/Scripts/CharacterSwapButton.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class CharacterSwapButton : MonoBehaviour {
 
+	// button number, set through inspector
 	public int bNum;
+	// button gameobject
 	public Button but;
 
+	// character switch game object, used to reference character switch script
 	public GameObject swapper;
 
 	// Use this for initialization
 	void Start () {
+		// add listener to this button
 		but = this.GetComponent<Button>();
 		but.onClick.AddListener(SwapCharacter);
 	}
@@ -23,6 +27,7 @@ public class CharacterSwapButton : MonoBehaviour {
 
 	void SwapCharacter()
 	{
+		// switch statement - set the active duo member depending on which button this is.
 		switch (bNum) {
 		case 1:
 			GameManager.manager.activeDuo [swapper.GetComponent<CharacterSwitch> ().charToSwap] = GameManager.manager.team [0];
