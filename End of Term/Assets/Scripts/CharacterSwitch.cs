d94f859a701661c8fc2405e412f4df9e38a883e8
Unknown <ak6429@nyu.edu>
2018-04-28 21:13:40 -0400

    commented

--- End of Term/Assets/Scripts/CharacterSwitch.cs
+++ End of Term/Assets/Scripts/CharacterSwitch.cs
@@ -5,42 +5,49 @@ using UnityEngine.UI;
 
 public class CharacterSwitch : MonoBehaviour {
 
+	// character name text objects
 	public Text Char1;
 	public Text Char2;
 	public Text Char3;
 	public Text Char4;
 
+	// character stats text objects
 	public Text Char1stats;
 	public Text Char2stats;
 	public Text Char3stats;
 	public Text Char4stats;
 
+	// character portrait images
 	public Image char1portrait;
 	public Image char2portrait;
 	public Image char3portrait;
 	public Image char4portrait;
 
+	// character selected images (the red squares)
 	public GameObject selected1;
 	public GameObject selected2;
 	public GameObject selected3;
 	public GameObject selected4;
 
+	// character select buttons
 	public Button b1;
 	public Button b2;
 	public Button b3;
 	public Button b4;
 
+	// which char to swap, corresponds to team index
 	public int charToSwap = 0;
 
 	// Use this for initialization
 	void Start () {
 
+		// set the characters' name field to their name
 		Char1.text = GameManager.manager.team [0].characterName;
 		Char2.text = GameManager.manager.team [1].characterName;
 		Char3.text = GameManager.manager.team [2].characterName;
 		Char4.text = GameManager.manager.team [3].characterName;
 
-
+		// fills the character description
 		Char1stats.text = FillDescription(GameManager.manager.team [0].attack, GameManager.manager.team [0].defense,GameManager.manager.team [0].spAttack, GameManager.manager.team [0].spDefense);
 		Char2stats.text = FillDescription(GameManager.manager.team [1].attack, GameManager.manager.team [1].defense,GameManager.manager.team [1].spAttack, GameManager.manager.team [1].spDefense);
 		Char3stats.text = FillDescription(GameManager.manager.team [2].attack, GameManager.manager.team [2].defense,GameManager.manager.team [2].spAttack, GameManager.manager.team [2].spDefense);
@@ -49,12 +56,13 @@ public class CharacterSwitch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// run the select heroes function
 		SelectedHeroes ();
-
 	}
 
 	string FillDescription(int attack, int defense, int specialA, int specialD)
 	{
+		// return this string to fill in character stats
 		return "Attack: " + attack + "\nDefense: " + defense + "\nSpecial Attack: " + specialA + "\nSpecial Defense: " + specialD;
 		Debug.Log ("Filled");
 	}
@@ -62,10 +70,14 @@ public class CharacterSwitch : MonoBehaviour {
 	void SelectedHeroes(){
 		for (int i = 0; i < 4; i++) {
 			Debug.Log (i);
+
+			// if either of the active duo members is equal to the team member at index 'i', mark the button text for that member as selected.
 			if (GameManager.manager.activeDuo [0] == GameManager.manager.team [i] || GameManager.manager.activeDuo [1] == GameManager.manager.team [i]) {
 				Debug.Log ("checK");
 				if (i == 0) {
+					// set the red box to true
 					selected1.gameObject.SetActive (true);
+					// set the button text to selected
 					b1.GetComponentInChildren<Text> ().text = "SELECTED";
 				}
 				if (i == 1) {
@@ -84,9 +96,12 @@ public class CharacterSwitch : MonoBehaviour {
 			}
 			else
 			{
+				// if the active duo is not equal to team member i then do this
 				Debug.Log ("checKed");
 				if (i == 0) {
+					// deactivate red box
 					selected1.gameObject.SetActive (false);
+					// set button text to deselected
 					b1.GetComponentInChildren<Text> ().text = "deselected";
 				}
 				if (i == 1) {
