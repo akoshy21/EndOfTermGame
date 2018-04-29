518e0e153288cda6cb3520dca9cf9617d83833d5
Unknown <rmh472@nyu.edu>
2018-04-28 21:35:02 -0400

    Turn Cycles After Swapping

--- End of Term/Assets/Scripts/CharacterSwapButton.cs
+++ End of Term/Assets/Scripts/CharacterSwapButton.cs
@@ -1,29 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-	{
-        //Cycle to Next Turn After Swapping
-        GameManager.manager.turnstate = GameManager.TurnState.Menu;
+public class CharacterSwapButton : MonoBehaviour
+{
 
-        if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo0)
-            GameManager.manager.curTurn = GameManager.CurrentTurn.ActiveDuo1;
-           
-        if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo1)
-            GameManager.manager.curTurn = GameManager.CurrentTurn.Enemy0;
+    public int bNum;
+    public Button but;
+
+    public GameObject swapper;
+
+    // Use this for initialization
+    void Start()
+    {
+        but = this.GetComponent<Button>();
+        but.onClick.AddListener(SwapCharacter);
+    }
+    // Update is called once per frame
+    void Update()
+{
+
+}
+
+void SwapCharacter()
+{
+    //Cycle to Next Turn After Swapping
+    GameManager.manager.turnstate = GameManager.TurnState.Menu;
+
+    if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo0)
+        GameManager.manager.curTurn = GameManager.CurrentTurn.ActiveDuo1;
+
+    if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo1)
+        GameManager.manager.curTurn = GameManager.CurrentTurn.Enemy0;
 
 
-        switch (bNum) {
-	{
-		// switch statement - set the active duo member depending on which button this is.
-		switch (bNum) {
-	{
-        //Cycle to Next Turn After Swapping
-        GameManager.manager.turnstate = GameManager.TurnState.Menu;
-
-        if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo0)
-            GameManager.manager.curTurn = GameManager.CurrentTurn.ActiveDuo1;
-           
-        if (GameManager.manager.curTurn == GameManager.CurrentTurn.ActiveDuo1)
-            GameManager.manager.curTurn = GameManager.CurrentTurn.Enemy0;
-
-
-        switch (bNum) {
-		// switch statement - set the active duo member depending on which button this is.
+    switch (bNum)
+    {
+        case 1:
+            GameManager.manager.activeDuo[swapper.GetComponent<CharacterSwitch>().charToSwap] = GameManager.manager.team[0];
+            return;
+        case 2:
+            GameManager.manager.activeDuo[swapper.GetComponent<CharacterSwitch>().charToSwap] = GameManager.manager.team[1];
+            return;
+        case 3:
+            GameManager.manager.activeDuo[swapper.GetComponent<CharacterSwitch>().charToSwap] = GameManager.manager.team[2];
+            return;
+        case 4:
+            GameManager.manager.activeDuo[swapper.GetComponent<CharacterSwitch>().charToSwap] = GameManager.manager.team[3];
+            return;
+        default:
+            return;
+    }
+}
+}
